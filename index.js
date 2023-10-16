72481e7efb903e74c8ace0db200b10752989335a
VEDANT JAIN <jainvedant1211@gmail.com>
2023-10-16 19:48:09 +0530

    initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const jsonfile = require("jsonfile");
+
+const moment = require("moment");
+const simpleGit = require("simple-git");
+
+const random = require("random");
+const FILE_PATH = "./data.json";
+
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit().push();
+  const x = random.int(0, 54);
+  const y = random.int(0, 6);
+  const DATE = moment()
+    .subtract(200, "d")
+    .add(x, "d")
+    .add(1, "w")
+    .add(1, "d")
+    .format();
+  const data = {
+    date: DATE,
+  };
+  console.log(DATE);
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n))
+      .push();
+  });
+};
+
+makeCommit(200);
